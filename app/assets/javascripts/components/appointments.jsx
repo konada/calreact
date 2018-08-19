1bf82c5708ad09f82ce0f65ce3a1c124b032404a
konada <konrad.cisak@gmail.com>
2018-08-19 12:55:42 +0200

    handling user input in form text fields and set state

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,23 @@
 var Appointments = React.createClass({
+  getInitialState: function() {
+    return {
+      appointments: this.props.appointments,
+      title: 'Team standup meeting',
+      appt_time: '25 January 2016 9am'
+    }
+  },
+
+  handleUserInput: function(obj) {
+    this.setState(obj);
+  },
+
   render: function() {
     return (
       <div>
-        <AppointmentForm />
-       <AppointmentsList appointments={this.props.appointments} />
+        <AppointmentForm input_title={this.state.title}
+          input_appt_time={this.state.appt_time}
+          onUserInput={this.handleUserInput} />
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
