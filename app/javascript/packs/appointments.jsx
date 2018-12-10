93bcd0f924b29deae26a65bd8c232d514c8b027d
konada <konrad.cisak@gmail.com>
2018-12-10 22:11:29 +0100

    Add model validations for appointment

--- app/javascript/packs/appointments.jsx
+++ app/javascript/packs/appointments.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom'
 import AppointmentForm from './appointment_form'
 import { AppointmentsList } from './appointments_list'
 import update from 'immutability-helper'
+import { FormErrors } from './FormErrors';
 
 export default class Appointments extends React.Component {
-  constructor (props) {
+  constructor (props, railsContext) {
     super(props)
     this.state = {
       appointments: this.props.appointments,
       title: 'Team standup meeting',
-      appt_time: '25 January 2016 9am'
+      appt_time: '',
+      formErrors: {}
     }
   }
 
@@ -24,8 +26,17 @@ export default class Appointments extends React.Component {
             {appointment: appointment})
           .done((data) => {
             this.addNewAppointment(data);
+            this.resetFormErrors();
+          })
+          .fail((response) => {
+            this.setState({formErrors: response.responseJSON})
           });
   }
+
+  resetFormErrors () {
+    this.setState({formErrors: {}})
+  }
+
 
   addNewAppointment (appointment) {
     const appointments = update(this.state.appointments, { $push: [appointment]});
@@ -39,6 +50,7 @@ export default class Appointments extends React.Component {
   render () {
     return (
       <div>
+        <FormErrors formErrors = {this.state.formErrors} />
         <AppointmentForm input_title={this.state.title}
           input_appt_time={this.state.appt_time}
           onUserInput={(obj) => this.handleUserInput(obj)}
